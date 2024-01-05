392f53f600accd8a66176549e3bcde12e2a9ab7d
Yen Nguyen <69042301+yen-nguyen34@users.noreply.github.com>
2024-01-05 07:12:50 +0700

    Update MemberServiceImpl.java

--- Nguyen Thi Yen_63200199_MLDL_Team/Backend/MLDLMemberManagement/src/main/java/com/project/service/MemberServiceImpl.java
+++ Nguyen Thi Yen_63200199_MLDL_Team/Backend/MLDLMemberManagement/src/main/java/com/project/service/MemberServiceImpl.java
@@ -18,74 +18,125 @@ import com.project.repository.MemberRepository;
 @Service
 public class MemberServiceImpl implements MemberService {
 
-	@Autowired
-	private MemberRepository memberRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
-	// sign-up
-	@Override
-	public Member signUpMember(MemberSaveDto memberSaveDto) {
-		LocalDateTime now = LocalDateTime.now();
-		Member member = new Member(memberSaveDto.getMemberId(), memberSaveDto.getMemberName(),
-				memberSaveDto.getMemberPwd(), memberSaveDto.getMemberPhone(), memberSaveDto.getMemberEmail(), now, now);
-		return memberRepository.save(member);
-	}
+    // sign-up
+    @Override
+    public Member signUpMember(MemberSaveDto memberSaveDto) {
+        LocalDateTime now = LocalDateTime.now();
+        Member member = new Member(memberSaveDto.getMemberId(), memberSaveDto.getMemberName(),
+                memberSaveDto.getMemberPwd(), memberSaveDto.getMemberPhone(), memberSaveDto.getMemberEmail(), now, now);
+        return memberRepository.save(member);
+    }
 
-	// list all members
-	@Override
-	public List<MemberDto> getAllMembers() {
-		List<Member> member = memberRepository.findAll();
-		List<MemberDto> memberDtoList = new ArrayList<>();
-		for (Member i : member) {
-			MemberDto memberDto = new MemberDto(i.getMemberNo(), i.getMemberId(), i.getMemberName(), i.getMemberPhone(),
-					i.getMemberEmail(), i.getSignUpDate(), i.getUpdateDate());
-			memberDtoList.add(memberDto);
-		}
+    // list all members
+    @Override
+    public List<MemberDto> getAllMembers() {
+        List<Member> member = memberRepository.findAll();
+        List<MemberDto> memberDtoList = new ArrayList<>();
+        for (Member i : member) {
+            MemberDto memberDto = new MemberDto(i.getMemberNo(), i.getMemberId(), i.getMemberName(), i.getMemberPhone(),
+                    i.getMemberEmail(), i.getSignUpDate(), i.getUpdateDate());
+            memberDtoList.add(memberDto);
+        }
 
-		return memberDtoList;
-	}
+        return memberDtoList;
+    }
 
-	// search members
-	@Override
-	public List<Member> searchMembers(MemberSearchDto memberSearchDto) {
-		return memberRepository.searchMembersbySQL(memberSearchDto.getMemberId(), memberSearchDto.getMemberName(),
-				memberSearchDto.getMemberPhone(), memberSearchDto.getStartDate(), memberSearchDto.getEndDate());
-	}
+    // search members
+    @Override
+    public List<Member> searchMembers(MemberSearchDto memberSearchDto) {
+        return memberRepository.searchMembersbySQL(memberSearchDto.getMemberId(), memberSearchDto.getMemberName(),
+                memberSearchDto.getMemberPhone(), memberSearchDto.getStartDate(), memberSearchDto.getEndDate());
+    }
 
-	// delete member
-	@Override
-	public Boolean deleteMember(long memberNo) {
-		if (memberRepository.existsById(memberNo)) {
-			memberRepository.deleteById(memberNo);
-		} else {
-			System.out.println("Member not found!");
-		}
-		return true;
-	}
+    @Override
+    public void exportSearchResultsToExcel(MemberSearchDto memberSearchDto, String filePath) {
+        List<MemberDto> memberDtoList = convertToMemberDtoList(searchMembers(memberSearchDto));
+    }
 
-	// get Member by memberNo
-	@Override
-	public Optional<Member> getMemberByNo(long memberNo) {
-		Optional<Member> member = memberRepository.findById(memberNo);
-		return member;
-	}
+    private List<MemberDto> convertToMemberDtoList(List<Member> members){
+        List<MemberDto> memberDtoList = new ArrayList<>();
+        for (Member i : members) {
+            MemberDto memberDto = new MemberDto(i.getMemberNo(), i.getMemberId(), i.getMemberName(), i.getMemberPhone(),
+                    i.getMemberEmail(), i.getSignUpDate(), i.getUpdateDate());
+            memberDtoList.add(memberDto);
+        }
+        return memberDtoList;
+    }
 
-	// update member by memberNo
-	@Override
-	public Member updateMember(MemberUpdateDto memberUpdateDto) {
-		Optional<Member> optionalMember = memberRepository.findById(memberUpdateDto.getMemberNo());
+    // delete member
+    @Override
+    public Boolean deleteMember(long memberNo) {
+        if (memberRepository.existsById(memberNo)) {
+            memberRepository.deleteById(memberNo);
+        } else {
+            System.out.println("Member not found!");
+        }
+        return true;
+    }
 
-		if (optionalMember.isPresent()) {
-			Member existingMember = optionalMember.get();
-			existingMember.setMemberName(memberUpdateDto.getMemberName());
-			existingMember.setMemberPwd(memberUpdateDto.getMemberPwd());
-			existingMember.setMemberPhone(memberUpdateDto.getMemberPhone());
-			existingMember.setMemberEmail(memberUpdateDto.getMemberEmail());
+    @Override
+    public Boolean checkDuplicateId(String memberId) {
+        return memberRepository.existsByMemberId(memberId);
+    }
 
-			return memberRepository.save(existingMember);
-		} else {
-			System.out.println("Member not found with memberNo: " + memberUpdateDto.getMemberNo());
-			return null;
-		}
-	}
+    @Override
+    public Boolean isValidPassword(String memberPwd) {
+        // At least one letter
+        String regexLetter = ".*[a-zA-Z].*";
+
+        // At least one digit
+        String regexDigit = ".*\\d.*";
+
+        // At least one special character
+        String regexSpecialChar = ".*[!@#$%^&*].*";
+
+        // 10-20 characters for one letter with one digit or one special character
+        String regexLength2Combinations = "^.{10,20}$";
+
+        // 8-20 characters for both letter, digit, and special character
+        String regexLength3Combinations = "^.{8,20}$";
+
+        // No more than 3 consecutive digits
+        String regexConsecutiveDigits = "^(?!.*\\d{3,})[a-zA-Z!@#$%^&*]*\\d*[1-9]\\d*[a-zA-Z!@#$%^&*]*$";
+
+        return ((memberPwd.matches(regexLetter) && memberPwd.matches(regexDigit) && !memberPwd.matches(regexSpecialChar) && memberPwd.matches(regexLength2Combinations)
+                && memberPwd.matches(regexConsecutiveDigits))
+                || (memberPwd.matches(regexLetter) && !memberPwd.matches(regexDigit) && memberPwd.matches(regexSpecialChar) && memberPwd.matches(regexLength2Combinations)
+                && memberPwd.matches(regexConsecutiveDigits))
+                || (!memberPwd.matches(regexLetter) && memberPwd.matches(regexDigit) && memberPwd.matches(regexSpecialChar) && memberPwd.matches(regexLength2Combinations)
+                && memberPwd.matches(regexConsecutiveDigits))
+                || (memberPwd.matches(regexLetter) && memberPwd.matches(regexDigit) &&
+                memberPwd.matches(regexSpecialChar) && memberPwd.matches(regexLength3Combinations) && memberPwd.matches(regexConsecutiveDigits)));
+    }
+
+
+    // get Member by memberNo
+    @Override
+    public Optional<Member> getMemberByNo(long memberNo) {
+        Optional<Member> member = memberRepository.findById(memberNo);
+        return member;
+    }
+
+    // update member by memberNo
+    @Override
+    public Member updateMember(MemberUpdateDto memberUpdateDto) {
+        Optional<Member> optionalMember = memberRepository.findById(memberUpdateDto.getMemberNo());
+
+        if (optionalMember.isPresent()) {
+            Member existingMember = optionalMember.get();
+            existingMember.setMemberName(memberUpdateDto.getMemberName());
+            existingMember.setMemberPwd(memberUpdateDto.getMemberPwd());
+            existingMember.setMemberPhone(memberUpdateDto.getMemberPhone());
+            existingMember.setMemberEmail(memberUpdateDto.getMemberEmail());
+
+            return memberRepository.save(existingMember);
+        } else {
+            System.out.println("Member not found with memberNo: " + memberUpdateDto.getMemberNo());
+            return null;
+        }
+    }
 
 }
